66695b3cde1223296f18a5a1aa4a0f2d678676c0
Justin Lindh <justinlindh@gmail.com>
2026-02-16 00:20:43 -0800

    Phase 3 Tasks 26-28: Routing data structures with neighbor table, routing table, RREQ dedup, reverse routes
    - components/routing/: neighbor table (upsert, purge, eviction), routing table
      (install with metric preference, maintenance with state transitions),
      RREQ dedup cache, reverse route table, compute_link_penalty
    - test/test_neighbor.c: 7 tests (init, add, update, purge, eviction, link penalty)
    - test/test_routing.c: 8 tests (init, install, metric update, maintenance, rreq dedup, reverse routes)
    - All 68 tests passing (53 existing + 15 new)

---
+++ test/test_neighbor.c
@@ -0,0 +1,73 @@
+#include "unity.h"
+#include "../components/routing/routing.c"
+
+static neighbor_table_t tbl;
+
+void setUp(void) { neighbor_init(&tbl); }
+void tearDown(void) {}
+
+void test_neighbor_init_empty(void) {
+    TEST_ASSERT_EQUAL(0, neighbor_count(&tbl));
+}
+
+void test_neighbor_add_and_lookup(void) {
+    neighbor_update(&tbl, 0xAABB, -70, 8, 0x1234, 1000);
+    neighbor_entry_t *e = neighbor_lookup(&tbl, 0xAABB);
+    TEST_ASSERT_NOT_NULL(e);
+    TEST_ASSERT_EQUAL(-70, e->rssi);
+    TEST_ASSERT_EQUAL(8, e->snr);
+    TEST_ASSERT_EQUAL(1, neighbor_count(&tbl));
+}
+
+void test_neighbor_update_existing(void) {
+    neighbor_update(&tbl, 0xAABB, -70, 8, 0x1234, 1000);
+    neighbor_update(&tbl, 0xAABB, -80, 5, 0x1234, 2000);
+    TEST_ASSERT_EQUAL(1, neighbor_count(&tbl));
+    neighbor_entry_t *e = neighbor_lookup(&tbl, 0xAABB);
+    TEST_ASSERT_EQUAL(-80, e->rssi);
+    TEST_ASSERT_EQUAL(5, e->snr);
+    TEST_ASSERT_EQUAL(2000, e->last_heard);
+}
+
+void test_neighbor_purge_expired(void) {
+    neighbor_update(&tbl, 1, -70, 8, 0, 1000);
+    neighbor_update(&tbl, 2, -70, 8, 0, 700000);
+    neighbor_purge(&tbl, 700000);
+    TEST_ASSERT_EQUAL(1, neighbor_count(&tbl));
+    TEST_ASSERT_NULL(neighbor_lookup(&tbl, 1));
+    TEST_ASSERT_NOT_NULL(neighbor_lookup(&tbl, 2));
+}
+
+void test_neighbor_full_evicts_oldest(void) {
+    for (uint32_t i = 0; i < MAX_NEIGHBORS; i++) {
+        neighbor_update(&tbl, i + 1, -70, 8, 0, 1000 + i);
+    }
+    TEST_ASSERT_EQUAL(MAX_NEIGHBORS, neighbor_count(&tbl));
+    /* Add one more; addr 1 (oldest at t=1000) should be evicted */
+    neighbor_update(&tbl, 0xFF, -60, 10, 0, 5000);
+    TEST_ASSERT_EQUAL(MAX_NEIGHBORS, neighbor_count(&tbl));
+    TEST_ASSERT_NULL(neighbor_lookup(&tbl, 1));
+    TEST_ASSERT_NOT_NULL(neighbor_lookup(&tbl, 0xFF));
+}
+
+void test_link_penalty_excellent(void) {
+    uint8_t p = compute_link_penalty(-60, 10);
+    TEST_ASSERT_LESS_OR_EQUAL(5, p);
+}
+
+void test_link_penalty_marginal(void) {
+    uint8_t p = compute_link_penalty(-120, -5);
+    TEST_ASSERT_GREATER_OR_EQUAL(30, p);
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_neighbor_init_empty);
+    RUN_TEST(test_neighbor_add_and_lookup);
+    RUN_TEST(test_neighbor_update_existing);
+    RUN_TEST(test_neighbor_purge_expired);
+    RUN_TEST(test_neighbor_full_evicts_oldest);
+    RUN_TEST(test_link_penalty_excellent);
+    RUN_TEST(test_link_penalty_marginal);
+    return UNITY_END();
+}
---
+++ test/test_routing.c
@@ -0,0 +1,88 @@
+#include "unity.h"
+#include "../components/routing/routing.c"
+
+static routing_table_t rt;
+
+void setUp(void) { route_init(&rt); }
+void tearDown(void) {}
+
+void test_route_init_empty(void) {
+    TEST_ASSERT_EQUAL(0, route_count(&rt));
+}
+
+void test_route_install_and_lookup(void) {
+    route_install(&rt, 0xDEAD, 0x0001, 3, 200, ROUTE_ACTIVE, 1000);
+    route_entry_t *e = route_lookup(&rt, 0xDEAD);
+    TEST_ASSERT_NOT_NULL(e);
+    TEST_ASSERT_EQUAL(0x0001, e->next_hop);
+    TEST_ASSERT_EQUAL(3, e->hop_count);
+    TEST_ASSERT_EQUAL(200, e->metric);
+    TEST_ASSERT_EQUAL(ROUTE_ACTIVE, e->state);
+}
+
+void test_route_update_better_metric(void) {
+    route_install(&rt, 0xDEAD, 0x0001, 3, 100, ROUTE_ACTIVE, 1000);
+    route_install(&rt, 0xDEAD, 0x0002, 2, 200, ROUTE_ACTIVE, 2000);
+    TEST_ASSERT_EQUAL(1, route_count(&rt));
+    route_entry_t *e = route_lookup(&rt, 0xDEAD);
+    TEST_ASSERT_EQUAL(200, e->metric);
+    TEST_ASSERT_EQUAL(0x0002, e->next_hop);
+}
+
+void test_route_maintenance_active_to_stale(void) {
+    route_install(&rt, 0xDEAD, 0x0001, 3, 200, ROUTE_ACTIVE, 1000);
+    route_maintenance(&rt, 1000 + ROUTE_ACTIVE_TIMEOUT_MS);
+    route_entry_t *e = route_lookup(&rt, 0xDEAD);
+    TEST_ASSERT_NOT_NULL(e);
+    TEST_ASSERT_EQUAL(ROUTE_STALE, e->state);
+}
+
+void test_route_maintenance_stale_removed(void) {
+    route_install(&rt, 0xDEAD, 0x0001, 3, 200, ROUTE_ACTIVE, 1000);
+    /* First make it stale */
+    route_maintenance(&rt, 1000 + ROUTE_ACTIVE_TIMEOUT_MS);
+    /* Then remove after STALE_TIMEOUT from last_confirmed */
+    route_maintenance(&rt, 1000 + ROUTE_STALE_TIMEOUT_MS);
+    TEST_ASSERT_EQUAL(0, route_count(&rt));
+}
+
+void test_route_unverified_state(void) {
+    route_install(&rt, 0xBEEF, 0x0001, 2, 150, ROUTE_UNVERIFIED, 1000);
+    route_entry_t *e = route_lookup(&rt, 0xBEEF);
+    TEST_ASSERT_NOT_NULL(e);
+    TEST_ASSERT_EQUAL(ROUTE_UNVERIFIED, e->state);
+}
+
+void test_rreq_dedup(void) {
+    rreq_dedup_t cache;
+    rreq_dedup_init(&cache);
+    TEST_ASSERT_FALSE(rreq_dedup_check_and_add(&cache, 42, 1000));
+    TEST_ASSERT_TRUE(rreq_dedup_check_and_add(&cache, 42, 1000));
+    /* After expiry */
+    TEST_ASSERT_FALSE(rreq_dedup_check_and_add(&cache, 42, 1000 + RREQ_DEDUP_EXPIRY_MS + 1));
+}
+
+void test_reverse_route(void) {
+    reverse_route_table_t rvt;
+    reverse_route_init(&rvt);
+    reverse_route_add(&rvt, 99, 0xAA, 1000);
+    reverse_route_t *r = reverse_route_lookup(&rvt, 99);
+    TEST_ASSERT_NOT_NULL(r);
+    TEST_ASSERT_EQUAL(0xAA, r->prev_hop);
+    /* Purge after expiry */
+    reverse_route_purge(&rvt, 1000 + REVERSE_ROUTE_EXPIRY_MS + 1);
+    TEST_ASSERT_NULL(reverse_route_lookup(&rvt, 99));
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_route_init_empty);
+    RUN_TEST(test_route_install_and_lookup);
+    RUN_TEST(test_route_update_better_metric);
+    RUN_TEST(test_route_maintenance_active_to_stale);
+    RUN_TEST(test_route_maintenance_stale_removed);
+    RUN_TEST(test_route_unverified_state);
+    RUN_TEST(test_rreq_dedup);
+    RUN_TEST(test_reverse_route);
+    return UNITY_END();
+}
