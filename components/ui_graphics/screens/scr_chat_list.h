a2de4b64ab907f9c2f6efcd8a4c8dea717921262
justin <justin@openclaw.openclaw.idiotica.org>
2026-02-19 14:46:37 -0800

    feat(ui_graphics): chat conversation list screen

---
+++ components/ui_graphics/screens/scr_chat_list.h
@@ -0,0 +1,10 @@
+#ifndef SCR_CHAT_LIST_H
+#define SCR_CHAT_LIST_H
+
+#include "lvgl.h"
+#include "scr_layout.h"
+
+void scr_chat_list_create(bramble_layout_t *layout);
+void scr_chat_list_refresh(bramble_layout_t *layout);
+
+#endif
