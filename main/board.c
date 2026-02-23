e25593e6b5bf86adb8cb61f359ba34968d65c1b2
Justin Lindh <justinlindh@gmail.com>
2026-02-23 11:05:43 -0800

    feat(board): add Heltec V4 board profile and selector wiring

--- main/board.c
+++ main/board.c
@@ -11,6 +11,8 @@
 /* Include the correct board config */
 #if defined(CONFIG_BRAMBLE_BOARD_TDECK_PLUS)
 #include "boards/tdeck_plus.h"
+#elif defined(CONFIG_BRAMBLE_BOARD_HELTEC_V4)
+#include "boards/heltec_v4.h"
 #elif defined(CONFIG_BRAMBLE_BOARD_HELTEC_V3) || !defined(CONFIG_BRAMBLE_BOARD_CUSTOM)
 #include "boards/heltec_v3.h"
 #endif
@@ -23,6 +25,8 @@ static bool s_initialized = false;
 const bramble_board_config_t *board_get_config(void) {
 #if defined(CONFIG_BRAMBLE_BOARD_TDECK_PLUS)
     return &board_tdeck_plus;
+#elif defined(CONFIG_BRAMBLE_BOARD_HELTEC_V4)
+    return &board_heltec_v4;
 #else
     return &board_heltec_v3;
 #endif
