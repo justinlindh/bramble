2e83d66a5acdc8cef1b7acb77d876453f993c165
Justin Lindh <justinlindh@gmail.com>
2026-02-23 03:14:35 -0800

    tune(mesh): reduce broadcast receipt attempts to 2

--- main/broadcast_delivery_receipt.c
+++ main/broadcast_delivery_receipt.c
@@ -5,7 +5,7 @@
 #define BROADCAST_RECEIPT_DELAY_BASE_MS      180u
 #define BROADCAST_RECEIPT_SLOT_SPACING_MS    140u
 #define BROADCAST_RECEIPT_SLOT_BUCKETS       16u
-#define BROADCAST_RECEIPT_RETRY_COUNT        3u
+#define BROADCAST_RECEIPT_RETRY_COUNT        2u
 
 bool mesh_should_emit_broadcast_delivery_receipt(uint32_t dest_addr) {
     return dest_addr == 0xFFFFFFFFu;
