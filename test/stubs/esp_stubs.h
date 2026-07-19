6e0595a3f7b250c2bc0623e43a9e5bef7917be13
justinlindh <justinlindh@gmail.com>
2026-07-18 23:44:53 -0700

    test: add ESP_LOGD/ESP_LOGV to the host esp_log stub

--- test/stubs/esp_stubs.h
+++ test/stubs/esp_stubs.h
@@ -16,8 +16,10 @@ typedef int esp_err_t;
             return -1;                                                                             \
     } while (0)
 
-#define ESP_LOGI(tag, fmt, ...) printf("I (%s) " fmt "\n", tag, ##__VA_ARGS__)
-#define ESP_LOGW(tag, fmt, ...) printf("W (%s) " fmt "\n", tag, ##__VA_ARGS__)
 #define ESP_LOGE(tag, fmt, ...) printf("E (%s) " fmt "\n", tag, ##__VA_ARGS__)
+#define ESP_LOGW(tag, fmt, ...) printf("W (%s) " fmt "\n", tag, ##__VA_ARGS__)
+#define ESP_LOGI(tag, fmt, ...) printf("I (%s) " fmt "\n", tag, ##__VA_ARGS__)
+#define ESP_LOGD(tag, fmt, ...) printf("D (%s) " fmt "\n", tag, ##__VA_ARGS__)
+#define ESP_LOGV(tag, fmt, ...) printf("V (%s) " fmt "\n", tag, ##__VA_ARGS__)
 
 #endif /* ESP_STUBS_H */
